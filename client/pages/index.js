e8d72a86c4f5cd29194c9d7a0251064120270325
Deukhwa <deukhwa.jeong@ratehub.ca>
2023-01-22 21:27:23 -0500

    add fetching data during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,27 @@
-const landing = () => {
-    return <div>Landing page</div>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+   
+    return <h1>Landing Page</h1>;
+  };
+
+    LandingPage.getInitialProps = async () => {
+        if (typeof window === 'undefined') {
+            // we are on the server
+            // requests should be made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+            const { data } = await axios.get(
+                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+                {
+                    headers: req.headers,
+                }
+            );
+            return data;
+        } else {
+            // we are on the browser
+            // requests can be made with a base url of ''
+            const { data } = await axios.get('/api/users/currentuser');
+            return data;
+        }
     };
 
-export default landing;
+export default LandingPage 
