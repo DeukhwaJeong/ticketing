998ccff244387001f51c179af1cfe627fb897d0c
Deukhwa <deukhwa.jeong@ratehub.ca>
2023-01-24 21:57:20 -0500

    add ticket routers and tests - show & new

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
 import cookieSession from 'cookie-session';
-
-import { errorHandler, NotFoundError } from '@deukhwatickets/common';
-
+import { errorHandler, NotFoundError, currentUser } from '@deukhwatickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 const app = express();
 
 app.set('trust proxy', true);
@@ -14,10 +13,14 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }))
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.get('*', async (req,res) => { 
     throw new NotFoundError();
 })
+
 app.use(errorHandler);
 
 export { app };
