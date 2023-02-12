d86b3052d1fed7a3ee7e7d2b4209aa2c7621c49c
Deukhwa <deukhwa.jeong@ratehub.ca>
2023-02-12 16:14:06 -0500

    add payments service

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,48 @@
+import { natsWrapper } from "../../../nats-wrapper";
+import { Order } from "../../../models/order";
+import { OrderCreatedEvent, OrderStatus } from "@deukhwatickets/common";
+import { OrderCreatedListener } from "../order-created-listener";
+import mongoose from "mongoose";
+
+const setup = async () => {
+    const listener = new OrderCreatedListener(natsWrapper.client);
+
+    const data: OrderCreatedEvent["data"] = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        status: OrderStatus.Created,
+        userId: new mongoose.Types.ObjectId().toHexString(),
+        expiresAt: "asdf",
+        ticket: {
+            id: new mongoose.Types.ObjectId().toHexString(),
+            price: 10
+        }
+    }
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    }
+
+    return { listener, data, msg };
+}
+
+it('replicates the order info', async () => {
+
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    const order = await Order.findById(data.id);
+
+    expect(order!.price).toEqual(data.ticket.price);
+
+})
+
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+})
